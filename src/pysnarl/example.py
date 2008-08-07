72107659fec7fc832f0b055af43eaca732521254
Phillip Pearson <pp@myelin.co.nz>
2008-08-07 12:00:36 +1200

    initial import

---
+++ src/pysnarl/example.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python
+import PySnarl
+if PySnarl.snGetVersion() != False:
+    (major, minor) = PySnarl.snGetVersion()
+    print "Found Snarl version",str(major)+"."+str(minor),"running."
+    PySnarl.snShowMessage("Test Message", "Hello World!")
+else:
+    print "Sorry Snarl does not appear to be running"
